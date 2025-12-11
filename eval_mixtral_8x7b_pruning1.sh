526aac91c9ac4abc191cbb1d70bb4aa75805b616
iankai <ykmao1515@outlook.com>
2025-12-11 22:20:56 +0000

    evaluate strcipts

---
+++ eval_mixtral_8x7b_pruning1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+save_dir="eval_results1/"
+global_record_file="eval_results1/eval_record_collection.csv"
+model="/workspace/MoE_pruning/mixtral8x7B_Instruct_pruning1experts"
+selected_subjects="all"
+gpu_util=0.8
+
+
+
+python evaluate_from_local.py \
+                 --selected_subjects $selected_subjects \
+                 --save_dir $save_dir \
+                 --model $model \
+                 --global_record_file $global_record_file \
+                 --gpu_util $gpu_util
+
